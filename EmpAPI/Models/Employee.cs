7f4cf504ccf07464e11864a83b92bae2024677c5
PHONG NGUYEN NGOC <nnphong@rd.local>
2020-05-29 14:10:03 +0700

    Add project files.

---
+++ EmpAPI/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmpAPI.Models
+{
+    public partial class Employee
+    {
+        public Guid EmployeeId { get; set; }
+        public string EmployeeCode { get; set; }
+        public string EmployeeName { get; set; }
+        public int? Age { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ EmpAPI/Models/EmployeedbContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmpAPI.Models
+{
+    public partial class EmployeedbContext : DbContext
+    {
+        public EmployeedbContext()
+        {
+        }
+
+        public EmployeedbContext(DbContextOptions<EmployeedbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;database=misa.nnphong", x => x.ServerVersion("10.4.12-mariadb"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("employee");
+
+                entity.HasComment("Bảng danh mục nhân viên");
+
+                entity.Property(e => e.EmployeeId)
+                    .HasColumnName("EmployeeID")
+                    .HasComment("PK")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.Address)
+                    .HasColumnType("varchar(255)")
+                    .HasComment("Quê quán")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.Age)
+                    .HasColumnType("int(11)")
+                    .HasComment("Tuổi");
+
+                entity.Property(e => e.CreatedBy)
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.EmployeeCode)
+                    .HasColumnType("varchar(20)")
+                    .HasComment("Mã nhân viên")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.EmployeeName)
+                    .HasColumnType("varchar(100)")
+                    .HasComment("Tên nhân viên")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.Gender)
+                    .HasColumnType("varchar(20)")
+                    .HasComment("Giới tính")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+
+                entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Phone)
+                    .HasColumnType("varchar(50)")
+                    .HasComment("Số điện thoại")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_unicode_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
